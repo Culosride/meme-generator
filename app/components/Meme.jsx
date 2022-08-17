5d601c378d51f401514f0d8e5549169b79981651
Andrea Spiga <drespriz@gmail.com>
2022-08-17 17:33:39 +0200

    adds meme generator function to button by setting url state

--- app/components/Meme.jsx
+++ app/components/Meme.jsx
@@ -1,10 +1,25 @@
+import memesData from "/home/andrea/meme-generator/memesData.js"
+import React from "react"
+
+
+
 export default function Meme() {
+  const [img, setImg] = React.useState("")
+
+  function getMemeImage() {
+    let randomUrl;
+    const array = memesData.data.memes
+    randomUrl = array[Math.floor(Math.random() * array.length)].url
+    setImg(randomUrl)
+  }
+
   return (
     <section>
       <div className="input-wrapper">
         <input type="text" placeholder="Upper text"/>
         <input type="text" placeholder="Lower text"/>
-        <button className="meme-btn">GENERATE MEME</button>
+        <button onClick={getMemeImage} className="meme-btn">GENERATE MEME</button>
+        <div className="meme-wrapper"><img src={img} className="meme-img" /></div>
       </div>
     </section>
   )
