fcb3416b7c87f6be9216d0b1f99b872a98200e3f
Andrea Spiga <drespriz@gmail.com>
2022-09-01 18:21:37 +0200

    connects text inputs status

--- app/components/Meme.jsx
+++ app/components/Meme.jsx
@@ -12,19 +12,53 @@ export default function Meme() {
 
   const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
+  console.log(img)
+
   function getMemeImage() {
-    const array = memesData.data.memes
-    const randomUrl = array[Math.floor(Math.random() * array.length)].url
+    const arrayMemes = allMemeImages.data.memes
+    const randomUrl = arrayMemes[Math.floor(Math.random() * arrayMemes.length)].url
     setImg(img => ({...img, randomImage: randomUrl}))
+  }
+
+  function handleChange(event) {
+    const {name, value} = event.target
+    setImg(prev => ({
+      ...prev,
+      [name]: value
+    }))
   }
 
   return (
     <section>
       <div className="input-wrapper">
-        <input type="text" placeholder="Upper text"/>
-        <input type="text" placeholder="Lower text"/>
-        <button onClick={getMemeImage} className="meme-btn">GENERATE MEME</button>
-        <div className="meme-wrapper"><img src={img.randomImage} className="meme-img" /></div>
+
+        <input
+          type="text"
+          placeholder="Upper text"
+          name="topText"
+          value={img.topText}
+          onChange={handleChange}
+        />
+
+        <input
+          type="text"
+          placeholder="Lower text"
+          name="bottomText"
+          value={img.bottomText}
+          onChange={handleChange}
+        />
+
+        <button
+          onClick={getMemeImage}
+          className="meme-btn">GENERATE MEME
+        </button>
+
+        <div className="meme-wrapper">
+          <img src={img.randomImage} className="meme-img" />
+            <h2 className="meme--text top">{img.topText}</h2>
+            <h2 className="meme--text bottom">{img.bottomText}</h2>
+        </div>
+
       </div>
     </section>
   )
